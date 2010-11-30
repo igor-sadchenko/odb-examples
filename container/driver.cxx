fe69aa1a2b29e577b86026f7bb56a19f5b3bdfef
Boris Kolpackov <boris@codesynthesis.com>
2010-11-30 18:13:45 +0200

    Add container example

---
+++ container/driver.cxx
@@ -0,0 +1,120 @@
+// file      : container/driver.cxx
+// author    : Boris Kolpackov <boris@codesynthesis.com>
+// copyright : not copyrighted - public domain
+
+#include <memory>   // std::auto_ptr
+#include <iostream>
+
+#include <odb/database.hxx>
+#include <odb/transaction.hxx>
+
+#include "database.hxx" // create_database
+
+#include "person.hxx"
+#include "person-odb.hxx"
+
+using namespace std;
+using namespace odb;
+
+void
+print (const person& p)
+{
+  cout << p.first () << " " << p.last () << endl;
+
+  // Print nicknames.
+  //
+  for (person::name_list::const_iterator i (p.nicknames ().begin ());
+       i != p.nicknames ().end (); ++i)
+  {
+    cout << "  nickname: " << *i << endl;
+  }
+
+  // Print emails.
+  //
+  for (person::email_set::const_iterator i (p.emails ().begin ());
+       i != p.emails ().end (); ++i)
+  {
+    cout << "  email: " << *i << endl;
+  }
+
+  // Print weights.
+  //
+  for (person::age_weight_map::const_iterator i (p.age_weight ().begin ());
+       i != p.age_weight ().end (); ++i)
+  {
+    cout << "  weight at " << i->first << ": " << i->second << endl;
+  }
+}
+
+int
+main (int argc, char* argv[])
+{
+  try
+  {
+    auto_ptr<database> db (create_database (argc, argv));
+
+    unsigned long id;
+
+    // Create a persistent person objects.
+    //
+    {
+      person joe ("Joe", "Dirt");
+
+      joe.nicknames ().push_back ("JD");
+      joe.nicknames ().push_back ("Squeaky");
+
+      joe.emails ().insert ("joe@example.com");
+      joe.emails ().insert ("joe.dirt@example.com");
+
+      joe.age_weight ()[5] = 15.6F;
+      joe.age_weight ()[10] = 23.3F;
+      joe.age_weight ()[15] = 29.8F;
+
+      transaction t (db->begin ());
+      id = db->persist (joe);
+      t.commit ();
+    }
+
+    // Load the object and print what we've got. Then change some
+    // information and update it in the database.
+    //
+    {
+      transaction t1 (db->begin ());
+      auto_ptr<person> j (db->load<person> (id));
+      t1.commit ();
+
+      print (*j);
+
+      // Ann another nickname.
+      //
+      j->nicknames ().push_back ("Cleaner");
+
+      // The joe@example.com email is no longer working.
+      //
+      j->emails ().erase ("joe@example.com");
+
+      // Update a weight sample.
+      //
+      j->age_weight ()[15] = 28.8F;
+
+      transaction t2 (db->begin ());
+      db->update (*j);
+      t2.commit ();
+    }
+
+    // Load and print the updated object.
+    //
+    {
+      transaction t (db->begin ());
+      auto_ptr<person> j (db->load<person> (id));
+      t.commit ();
+
+      print (*j);
+    }
+  }
+  catch (const odb::exception& e)
+  {
+    cerr << e.what () << endl;
+    return 1;
+  }
+}
---
+++ container/person.hxx
@@ -0,0 +1,108 @@
+// file      : container/person.hxx
+// author    : Boris Kolpackov <boris@codesynthesis.com>
+// copyright : not copyrighted - public domain
+
+#ifndef PERSON_HXX
+#define PERSON_HXX
+
+#include <set>
+#include <map>
+#include <vector>
+#include <string>
+
+#include <odb/core.hxx>
+
+#pragma db object
+class person
+{
+public:
+  person (const std::string& first,
+          const std::string& last)
+      : first_ (first), last_ (last)
+  {
+  }
+
+  const std::string&
+  first () const
+  {
+    return first_;
+  }
+
+  const std::string&
+  last () const
+  {
+    return last_;
+  }
+
+  // Nicknames.
+  //
+  typedef std::vector<std::string> name_list;
+
+  const name_list&
+  nicknames () const
+  {
+    return nicknames_;
+  }
+
+  name_list&
+  nicknames ()
+  {
+    return nicknames_;
+  }
+
+  // Emails.
+  //
+  typedef std::set<std::string> email_set;
+
+  const email_set&
+  emails () const
+  {
+    return emails_;
+  }
+
+  email_set&
+  emails ()
+  {
+    return emails_;
+  }
+
+  // Age-to-weight map.
+  //
+  typedef std::map<unsigned short, float> age_weight_map;
+
+  const age_weight_map&
+  age_weight () const
+  {
+    return age_weight_;
+  }
+
+  age_weight_map&
+  age_weight ()
+  {
+    return age_weight_;
+  }
+
+public:
+  unsigned long
+  id () const
+  {
+    return id_;
+  }
+
+private:
+  friend class odb::access;
+
+  person () {}
+
+  #pragma db id auto
+  unsigned long id_;
+
+  std::string first_;
+  std::string last_;
+
+  name_list nicknames_;
+  email_set emails_;
+  age_weight_map age_weight_;
+};
+
+#endif // PERSON_HXX
